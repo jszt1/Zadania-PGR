121cc088c13825848c461347e368d534add2bafc
jszt1 <jozefsztabinski@gmail.com>
2024-04-12 22:18:15 +0200

    Update stack

---
+++ ZAD5/main.cpp
@@ -0,0 +1,11 @@
+#include <ncurses.h>
+
+#include <iostream>
+
+int main() {
+  initscr();                 // 1
+  printw("Hello World !!!"); // 2
+  getch();                   // 3
+  endwin();                  // 4
+  return 0;
+}
